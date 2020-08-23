5a776b376e492ec7576b9406455c19bfe36e7059
yuchiXiong <yuchi.xiong@foxmail.com>
2020-08-23 10:59:58 +0800

    关闭自动化部署中的静态资源打包阶段（项目暂无前端需要打包）

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,8 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    # * 项目暂不包含前端
+    # invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
