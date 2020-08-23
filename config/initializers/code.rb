c406f6588bfe5939028c49dc9307b2d3e7c27e3d
yuchiXiong <505831526@qq.com>
2020-08-23 09:45:58 +0800

    细化业务码

--- config/initializers/code.rb
+++ config/initializers/code.rb
@@ -1,8 +1,8 @@
 module Code
   Success = 8888
 
-  Parameter_Missing = 8400
-  Account_Or_Password_Not_Match = 8400
+  Parameter_Missing = 84000
+  Account_Or_Password_Not_Match = 84001
   Unauthorized_Expired = 8401
   Unauthorized_Error = 8402
   Access_Denied = 8403
