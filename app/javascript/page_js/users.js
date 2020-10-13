0ce279c0a35c0d66aabc0d0e0daa8b05f29b757b
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-13 21:00:54 +0800

    添加注释，修改回到顶部按钮的样式与交互效果

--- app/javascript/page_js/users.js
+++ app/javascript/page_js/users.js
@@ -7,16 +7,19 @@ dayjs.extend(relativeTime);
 
 $(document).on("turbolinks:load", () => {
 
+    // * .format_date为后端created_at格式时间，自动将其格式化
     const formatDateContainer = $('.format_date');
     for (let i = 0; i < formatDateContainer.length; i++) {
         const current = $(formatDateContainer[i]);
         current.text(dayjs(current.text()).fromNow());
     }
 
+    // * 回到顶部
     $('.to_top_btn').click(() => {
         $('html, body').animate({scrollTop: 0}, 450)
     });
 
+    // * 监听滚动条并隐藏/显示回到顶部按钮
     $(document).on("scroll", () => {
         if ($(document).scrollTop() > 200) {
             $('.to_top_btn').fadeIn(300);
