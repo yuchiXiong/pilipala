c8d53fa5458e735d757804949758e9d2eebf8560
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-13 21:47:52 +0800

    添加跳转链接，增加博客详情页，调整markdown解析器的配置

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,8 +3,25 @@ require 'redcarpet/render_strip'
 
 module BlogsHelper
 
+  DEFAULT_OPTIONS = {
+    no_styles:                    true,
+    hard_wrap:                    true,
+    autolink:                     true,
+    fenced_code_blocks:           true,
+    strikethrough:                true,
+    underline:                    true,
+    superscript:                  false,
+    footnotes:                    false,
+    highlight:                    false,
+    tables:                       true,
+    lax_spacing:                  true,
+    space_after_headers:          true,
+    disable_indented_code_blocks: true,
+    no_intra_emphasis:            true
+  }
+
   def render_markdown(content)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, DEFAULT_OPTIONS)
     markdown.render(content)
   end
 
@@ -12,4 +29,5 @@ module BlogsHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
     markdown.render(content)
   end
+
 end
