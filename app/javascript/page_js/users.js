4f499fd3d5d3fde2a53d3d513027ff2faca0a9fe
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-13 20:47:55 +0800

    调整页脚样式，调整用户页右侧个人简介与文集列表样式，抽离博客item视图片段，用户页添加回到顶部按钮

--- app/javascript/page_js/users.js
+++ app/javascript/page_js/users.js
@@ -6,9 +6,22 @@ dayjs.locale('zh-cn');
 dayjs.extend(relativeTime);
 
 $(document).on("turbolinks:load", () => {
+
     const formatDateContainer = $('.format_date');
     for (let i = 0; i < formatDateContainer.length; i++) {
         const current = $(formatDateContainer[i]);
         current.text(dayjs(current.text()).fromNow());
     }
+
+    $('.to_top_btn').click(() => {
+        $('html, body').animate({scrollTop: 0}, 450)
+    });
+
+    $(document).on("scroll", () => {
+        if ($(document).scrollTop() > 200) {
+            $('.to_top_btn').fadeIn(300);
+        } else {
+            $('.to_top_btn').fadeOut(300);
+        }
+    })
 });
