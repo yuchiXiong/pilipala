ce384a4d6b69d4c72912dbae22efa18723701e4d
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-15 22:36:02 +0800

    调整一些markdown的样式，博客详情页面增加用户信息，动态渲染文章目录并使之可以滚动到对应章节

--- app/javascript/page_js/blogs.js
+++ app/javascript/page_js/blogs.js
@@ -1,8 +1,48 @@
 import hljs from 'highlight.js';
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/zh-cn';
+
+dayjs.locale('zh-cn');
+dayjs.extend(relativeTime);
 
 $(document).on("turbolinks:load", () => {
     const codeBlocks = $('.markdown-body pre>code');
     for (let i = 0; i < codeBlocks.length; i++) {
         hljs.highlightBlock(codeBlocks[i]);
     }
+
+    // * .format_date为后端created_at格式时间，自动将其格式化
+    const formatDateContainer = $('.format_date');
+    for (let i = 0; i < formatDateContainer.length; i++) {
+        const current = $(formatDateContainer[i]);
+        current.text(dayjs(current.text()).fromNow());
+    }
+
+    // * 生成目录并添加锚点
+    $("article h1," +
+        "article h2," +
+        "article h3," +
+        "article h4," +
+        "article h5," +
+        "article h6").each(function (i, item) {
+
+        const tag = $(item).get(0).localName;
+        $(item).attr("id", "toc" + i);
+        $("#toc").append('<a class="toc_' + tag + '" href="#toc' + i + '">' + $(this).text() + '</a></br>');
+        $(".toc_h1").css("margin-left", 0);
+        $(".toc_h2").css("margin-left", 20);
+        $(".toc_h3").css("margin-left", 40);
+        $(".toc_h4").css("margin-left", 60);
+        $(".toc_h5").css("margin-left", 80);
+        $(".toc_h6").css("margin-left", 100);
+    });
+
+    $("#toc > a").each((index, item) => {
+        $(item).click(e => {
+            e.preventDefault();
+            $('html, body').animate({scrollTop: $($(item).attr('href')).offset().top - 80}, 450);
+        })
+    })
+
 });
--- app/javascript/page_js/users.js
+++ app/javascript/page_js/users.js
@@ -1,18 +1,4 @@
-import dayjs from "dayjs";
-import relativeTime from 'dayjs/plugin/relativeTime';
-import 'dayjs/locale/zh-cn';
-
-dayjs.locale('zh-cn');
-dayjs.extend(relativeTime);
-
 $(document).on("turbolinks:load", () => {
-
-    // * .format_date为后端created_at格式时间，自动将其格式化
-    const formatDateContainer = $('.format_date');
-    for (let i = 0; i < formatDateContainer.length; i++) {
-        const current = $(formatDateContainer[i]);
-        current.text(dayjs(current.text()).fromNow());
-    }
 
     // * 回到顶部
     $('.to_top_btn').click(() => {
