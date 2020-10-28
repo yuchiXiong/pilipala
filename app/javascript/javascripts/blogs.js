f6a0f929ae27baaeb36c4ac6f8a25a238ee60549
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-28 22:09:14 +0800

    在rjs时添加闭包，博客详情页增加悬浮菜单栏，底部增加许可协议

--- app/javascript/javascripts/blogs.js
+++ app/javascript/javascripts/blogs.js
@@ -8,7 +8,7 @@ $(document).on("turbolinks:load", () => {
         $(document).scroll(() => {
             if ($(document).scrollTop() >= $(document).height() - $(window).height()) {
                 const loadMoreBtn = $('#load_more');
-                if (!loadMoreBtn.data('loading')) {
+                if ( !loadMoreBtn.data('loading')) {
                     document.querySelector('#load_more').click();
                     loadMoreBtn.data('loading', true);
                 }
@@ -29,8 +29,8 @@ $(document).on("turbolinks:load", () => {
         for (let i = 0; i < formatDateContainer.length; i++) {
             const current = $(formatDateContainer[i]);
             const formatDate = dayjs(current.text());
-            current.attr({title: formatDate.fromNow()});
-            current.text(formatDate.format('YYYY-MM-DD HH:mm:ss'));
+            current.attr({title: formatDate.fromNow()})
+                   .text(formatDate.format('YYYY-MM-DD HH:mm:ss'));
         }
 
         // * 生成目录并添加锚点
@@ -44,14 +44,11 @@ $(document).on("turbolinks:load", () => {
             const tag = $(item).get(0).localName;
             $(item).attr("id", "toc" + i);
             $("#toc").append('<a class="toc_' + tag + '" href="#toc' + i + '">' + $(this).text() + '</a></br>');
-            $(".toc_h1").css("margin-left", 0);
-            $(".toc_h2").css("margin-left", 20);
-            $(".toc_h3").css("margin-left", 40);
-            $(".toc_h4").css("margin-left", 60);
-            $(".toc_h5").css("margin-left", 80);
-            $(".toc_h6").css("margin-left", 100);
-        });
 
+        });
+        for (let i = 1; i < 7; i++) {
+            $(`.toc_h${i}`).css("margin-left", (i - 1) * 20);
+        }
     }
 
     // * 添加滚动跳转
@@ -59,7 +56,7 @@ $(document).on("turbolinks:load", () => {
         $(item).click(e => {
             e.preventDefault();
             $('html, body').animate({scrollTop: $($(item).attr('href')).offset().top - 80}, 450);
-        })
-    })
+        });
+    });
 
 });
