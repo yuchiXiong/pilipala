0ba3bf069c06d618e463ede355725475b6f33519
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-30 11:47:24 +0800

    把node_modules设置为部署共享目录

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,10 @@ set :rails_env, 'production'
 # * file settings
 set :puma_config, -> { "#{fetch(:current_path)}/config/puma.rb" }
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/master.key', 'config/credentials.yml.enc')
+set :compiled_asset_path, 'public/assets'
+set :packs_path, 'public/packs'
+set :node_modules_path, 'node_modules'
+set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/cache', fetch(:compiled_asset_path), fetch(:packs_path), fetch(:node_modules_path))
 
 task :remote_environment do
   invoke :'rvm:use', 'ruby-2.5.7'
