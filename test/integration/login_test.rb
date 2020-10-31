2a546f4189e721c1f6b6cc436787f5be4009d75c
yuchiXiong <yuchi.xiong@foxmail.com>
2020-10-31 22:55:25 +0800

    编写了一个自动化系统测试

---
+++ test/integration/login_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+class LoginTest < ActionDispatch::IntegrationTest
+  test "login success" do
+  end
+end
---
+++ test/system/devises_test.rb
@@ -0,0 +1,28 @@
+require "application_system_test_case"
+
+class DevisesTest < ApplicationSystemTestCase
+  # * 登陆
+  test "visiting the index" do
+    ActionController::Base.stub_any_instance(:verify_rucaptcha?, true) do
+      visit new_user_session_url
+
+      assert_title '登陆 - SmallBook'
+
+      assert_selector ".card > h4", text: "登陆"
+
+      assert_selector "form.new_user input#user_email"
+      assert_selector "form.new_user input#user_password"
+      assert_selector "form.new_user img"
+      assert_selector "form.new_user input[type=submit]"
+
+      within("#new_user") do
+        fill_in 'user[email]', with: 'yuchi.xiong@foxmail.com'
+        fill_in 'user[password]', with: '123456'
+
+        click_button '登陆'
+      end
+
+    end
+
+  end
+end
