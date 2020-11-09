4df1eba8e689fcaa69deda70e37d19cc40af23b0
yuchiXiong <yuchi.xiong@foxmail.com>
2020-11-09 21:21:04 +0800

    支持修改头像，添加默认头像

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require 'ali/oss'
+
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,6 +12,14 @@ class User < ApplicationRecord
 
   def resource_errors
     errors.full_messages
+  end
+
+  def oss_avatar
+    if avatar.file.nil?
+      'https://assets-blog-xiongyuchi.oss-cn-beijing.aliyuncs.com/avatars/dog.jpg'
+    else
+      "https://assets-blog-xiongyuchi.oss-cn-beijing.aliyuncs.com#{avatar.url}"
+    end
   end
 
 end
---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,37 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  # * 保存前记录文件cache id
+  before :store, :remember_cache_id
+  # * 保存后将文件上传至OSS并删除本地的文件
+  after :store, :save_to_ali_oss
+  # * 保存后删除文件cache
+  after :store, :delete_tmp_dir
+
+  storage :file
+
+  def store_dir
+    "avatars/#{Rails.env}/#{model.id}/"
+  end
+
+  # * 将文件上传至OSS
+  def save_to_ali_oss(file)
+    oss    = Ali::Oss.new
+    client = oss.client
+    bucket = client.get_bucket('assets-blog-xiongyuchi')
+    bucket.put_object("#{store_dir}#{filename}", file: path)
+    File.delete(path)
+  end
+
+  # * 记录文件cache id
+  def remember_cache_id(new_file)
+    @cache_id_was = cache_id
+  end
+
+  # * 删除文件cache
+  def delete_tmp_dir(new_file)
+    if @cache_id_was.present? && @cache_id_was =~ /\A[\d]+\-[\d]+(\-[\d]{4})?\-[\d]{4}\z/
+      FileUtils.rm_rf(File.join(root, cache_dir, @cache_id_was))
+    end
+  end
+
+end
