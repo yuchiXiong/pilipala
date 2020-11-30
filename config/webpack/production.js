c5e388b2e72e53953b33e2990dd31d1a1569ddc9
yuchiXiong <yuchi.xiong@foxmail.com>
2020-11-30 17:50:16 +0800

    正式环境关闭devtool

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const extendConfig = {
+    devtool: false
+};
+
+environment.config.merge(extendConfig);
+
 module.exports = environment.toWebpackConfig()
