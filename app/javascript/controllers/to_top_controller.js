c6688ee3e60b7e65215f575090f435aee2fcc89b
yuchiXiong <yuchi.xiong@foxmail.com>
2020-12-01 22:39:32 +0800

    替换一个回到顶部的icon，微调回到顶部组件的样式，微调页面的圆角风格

--- app/javascript/controllers/to_top_controller.js
+++ app/javascript/controllers/to_top_controller.js
@@ -8,9 +8,11 @@ export default class extends Controller {
     toggleVisible() {
         const el = this.element;
         if ($(document).scrollTop() > 200) {
-            $(el).fadeIn(450);
+            // $(el).fadeIn(450);
+            $(el).addClass('show_to_top ');
         } else {
-            $(el).fadeOut(450);
+            // $(el).fadeOut(450);
+            $(el).removeClass('show_to_top');
         }
     }
 }
