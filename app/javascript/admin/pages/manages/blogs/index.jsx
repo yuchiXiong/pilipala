2b98c17f760a4cf98a95cbf5efd0279b1c6379ae
yuchiXiong <yuchi.xiong@foxmail.com>
2020-12-28 19:09:27 +0800

    管理员可在用户菜单中看到后台入口

--- app/javascript/admin/pages/manages/blogs/index.jsx
+++ app/javascript/admin/pages/manages/blogs/index.jsx
@@ -1,71 +1,80 @@
 import React from 'react';
-import {Table, Tooltip, Typography} from 'antd';
+import {Table, Tooltip, Typography, Drawer, Button} from 'antd';
 import {Blog} from '../../../utils/api';
 
 const {Paragraph} = Typography;
 
-const columns = [
-    {
-        title: 'Id',
-        dataIndex: 'id',
-        sorter: true
-    },
-    {
-        title: '标题',
-        dataIndex: 'title',
-        render: text => <Tooltip title={text}>
-            <Paragraph ellipsis>{text}</Paragraph>
-        </Tooltip>,
-        width: '10%'
-    },
-    {
-        title: '内容',
-        dataIndex: 'content',
-        // render: (text) => <a target="_blank" href={`/blogs/${record.id}`}>原文</a>
-    },
-    {
-        title: '封面',
-        dataIndex: 'cover',
-    },
-    {
-        title: '作者',
-        dataIndex: 'user_id',
-    },
-    {
-        title: '发布状态',
-        dataIndex: 'released',
-    },
-    {
-        title: '删除状态',
-        dataIndex: 'discarded_at',
-    },
-    {
-        title: '审核结果',
-        dataIndex: 'scan_result',
-    },
-    {
-        title: '阅读数',
-        dataIndex: 'reads_count',
-    },
-    {
-        title: '点赞数',
-        dataIndex: 'likes_count',
-    },
-    {
-        title: '评论数',
-        dataIndex: 'comments_count',
-    },
-];
-
 class Blogs extends React.Component {
-    state = {
-        data: [],
-        pagination: {
-            current: 1,
-            pageSize: 10,
-        },
-        loading: false,
-    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            blogs: [],
+            pagination: {
+                current: 1,
+                pageSize: 10,
+            },
+            loading: false,
+            showBlogContent: false,
+            currentBlog: -1
+        };
+        this.columns = [
+            {
+                title: 'Id',
+                dataIndex: 'id',
+                sorter: true
+            },
+            {
+                title: '标题',
+                dataIndex: 'title',
+                render: text => <Tooltip title={text}>
+                    <Paragraph ellipsis>{text}</Paragraph>
+                </Tooltip>,
+                width: '10%'
+            },
+            {
+                title: '内容',
+                dataIndex: 'content',
+                render: (text, record) => <Button type='link'
+                                                  onClick={() => this.showBlogContent(record.id)}>
+                    预览
+                </Button>
+            },
+            {
+                title: '封面',
+                dataIndex: 'cover',
+            },
+            {
+                title: '作者',
+                dataIndex: 'user_id',
+            },
+            {
+                title: '发布状态',
+                dataIndex: 'released',
+            },
+            {
+                title: '删除状态',
+                dataIndex: 'discarded_at',
+            },
+            {
+                title: '审核结果',
+                dataIndex: 'scan_result',
+            },
+            {
+                title: '阅读数',
+                dataIndex: 'reads_count',
+            },
+            {
+                title: '点赞数',
+                dataIndex: 'likes_count',
+            },
+            {
+                title: '评论数',
+                dataIndex: 'comments_count',
+            },
+        ];
+        this.showBlogContent = this.showBlogContent.bind(this);
+    }
 
     componentDidMount() {
         const {pagination} = this.state;
@@ -86,20 +95,36 @@ class Blogs extends React.Component {
         Blog.index(params).then(data => {
             this.setState({
                 loading: false,
-                data: data.blogs,
+                blogs: data.blogs,
                 pagination: data.pagination
             })
         });
     };
 
+    showBlogContent(id) {
+        this.setState({
+            showBlogContent: true,
+            currentBlog: id
+        });
+    }
+
     render() {
-        const {data, pagination, loading} = this.state;
+        const {blogs, pagination, loading} = this.state;
         return (
             <>
+                <Drawer
+                    title={this.state.blogs.filter(item => item.id === this.state.currentBlog)[0].title}
+                    width={720}
+                    closable={false}
+                    onClose={() => this.setState({showBlogContent: false})}
+                    visible={this.state.showBlogContent}
+                >
+                    {this.state.currentBlog}
+                </Drawer>
                 <Table
-                    columns={columns}
+                    columns={this.columns}
                     rowKey={record => record.id}
-                    dataSource={data}
+                    dataSource={blogs}
                     pagination={pagination}
                     loading={loading}
                     onChange={this.handleTableChange}
