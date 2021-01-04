02a5ea89d581c1c401a678c86b9ba5936ad4b62d
yuchiXiong <yuchi.xiong@foxmail.com>
2021-01-04 23:23:45 +0800

    调整修改个人信息页的布局

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :verify_captcha, only: :create
+  layout 'application', only: :edit
 
   # GET /resource/sign_up
   # def new
