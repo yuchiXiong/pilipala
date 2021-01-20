a84072d164bc84f301f29ff89a8c80952839d112
yuchiXiong <yuchi.xiong@foxmail.com>
2021-01-20 21:58:09 +0800

    主页右侧添加热门文章模块

--- app/javascript/src/pages/home/index.jsx
+++ app/javascript/src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Row, Col, Carousel} from 'antd';
+import {Link} from 'react-router-dom';
+import {Row, Col, Carousel, Divider, List, Typography} from 'antd';
 import isomorphicProps from "../../containers/isomorphicProps";
 
 import default1 from '../../assets/images/default1.png';
@@ -7,6 +8,7 @@ import default2 from '../../assets/images/default2.png';
 
 import styles from './index.module.scss';
 import BlogList from "../../components/blog-list";
+import {LikeOutlined, MessageOutlined, ReadOutlined, UserOutlined} from "@ant-design/icons";
 
 const {Title} = Typography;
 
@@ -29,7 +31,29 @@ class Home extends React.PureComponent {
                         <BlogList dataSource={this.props.blogs}/>
                     </Col>
                     <Col span={5} offset={1}>
-                        right
+                        <Title level={5} className={styles.tag}>热门作者</Title>
+                        <List
+                            size="large"
+                            itemLayout="vertical"
+                            bordered={false}
+                            dataSource={this.props.hots}
+                            className={styles.hotsAuthor}
+                            renderItem={item => <List.Item actions={[
+                                <p className={styles.action}>阅读 {item.readsCount}</p>,
+                                <p className={styles.action}>喜欢 {item.likesCount}</p>
+                            ]}>
+                                <Link to={`/blogs/${item.id}`}>{item.title}</Link>
+                            </List.Item>}
+                        />
+                        {/*<Title level={5} className={styles.tag}>大家都在看</Title>*/}
+                        {/*<List*/}
+                        {/*    size="large"*/}
+                        {/*    bordered={false}*/}
+                        {/*    dataSource={this.props.hots}*/}
+                        {/*    renderItem={item => <List.Item>*/}
+                        {/*        <Link to={`/blogs/${item.id}`}>{item.title}</Link>*/}
+                        {/*    </List.Item>}*/}
+                        {/*/>*/}
                     </Col>
                 </Row>
             </Col>
