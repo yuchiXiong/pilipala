616229d1adada776bb37a2b20b996be202415386
yuchiXiong <yuchi.xiong@foxmail.com>
2021-08-04 23:43:05 +0800

    移除掉webpacker目录下的入口文件

--- app/javascript/packs/stimulus.js
+++
@@ -1,7 +0,0 @@
-// src/application.js
-import {Application} from "stimulus"
-import {definitionsFromContext} from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const controllers = import.meta.globEager('../**/*_controller.js')
-application.load(definitionsFromContext(context))
