906f450e573c4f6b3df592c5db66ee2dead46e5f
yuchiXiong <yuchi.xiong@foxmail.com>
2021-09-15 00:11:42 +0800

    新增一个动态计算的目录栏目，并支持点击滚动和滚动自动定位；

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -3,7 +3,7 @@ console.log('Vite ⚡️ Rails');
 import Turbolinks from 'turbolinks';
 import TurbolinksPerfetch from 'turbolinks-prefetch';
 import Alpine from 'alpinejs';
-import '@rails/ujs';
+import Rails from '@rails/ujs';
 import dayjs from "dayjs";
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/zh-cn';
@@ -12,13 +12,16 @@ import 'dayjs/locale/zh-cn';
 
 import.meta.globEager('./**/*_channel.js');
 
+window.markdownTitleSelector = '.markdown-article > h1, .markdown-article > h2, .markdown-article > h3';
+
 Turbolinks.start();
 TurbolinksPerfetch.start();
+window.Rails = Rails;
+Rails.start();
 window.Alpine = Alpine;
 Alpine.start();
 // ActiveStorage.start();
 
 dayjs.locale('zh-cn');
 dayjs.extend(relativeTime);
-
 // global.dayjs = dayjs;
